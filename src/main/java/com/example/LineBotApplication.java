38f75b8af8e7316b9cb5d3cc7c53c2e738a17a9d
渡部保 <tamotsu917@yahoo.co.jp>
2017-06-12 00:42:16 +0900

    NINTH COMMIT

--- src/main/java/com/example/LineBotApplication.java
+++ src/main/java/com/example/LineBotApplication.java
@@ -1,13 +1,52 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.service.ConvertDayOfTheWeekService;
+import com.example.service.GarbageScheduleService;
+import com.example.service.StickMessageService;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 @SpringBootApplication
+@LineMessageHandler
 public class LineBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LineBotApplication.class, args);
 	}
-	
+	@Autowired
+    private ConvertDayOfTheWeekService cdService;
+
+	@Autowired
+    private GarbageScheduleService gsService;
+
+	@Autowired
+    private StickMessageService smService;
+
+	@EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		int dayOfTheWeek = cdService.convert(event.getMessage().getText());
+		if (dayOfTheWeek == -1) {
+			smService.getRandomMessage();
+		}
+		return gsService.getMessage(dayOfTheWeek);
+    }
+    
+    @EventMapping
+    public Message handleStickerMessage(MessageEvent<StickerMessageContent> event) {
+    	return smService.getRandomMessage();
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }
--- src/main/java/com/example/controller/LineBotController.java
+++ src/main/java/com/example/controller/LineBotController.java
@@ -14,34 +14,34 @@ import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
-@LineMessageHandler
+//@LineMessageHandler
 public class LineBotController {
 
-	@Autowired
-    private ConvertDayOfTheWeekService cdService;
-
-	@Autowired
-    private GarbageScheduleService gsService;
-
-	@Autowired
-    private StickMessageService smService;
-
-	@EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-		int dayOfTheWeek = cdService.convert(event.getMessage().getText());
-		if (dayOfTheWeek == -1) {
-			smService.getRandomMessage();
-		}
-		return gsService.getMessage(dayOfTheWeek);
-    }
-    
-    @EventMapping
-    public Message handleStickerMessage(MessageEvent<StickerMessageContent> event) {
-    	return smService.getRandomMessage();
-    }
-
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-        System.out.println("event: " + event);
-    }
+//	@Autowired
+//    private ConvertDayOfTheWeekService cdService;
+//
+//	@Autowired
+//    private GarbageScheduleService gsService;
+//
+//	@Autowired
+//    private StickMessageService smService;
+//
+//	@EventMapping
+//    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+//		int dayOfTheWeek = cdService.convert(event.getMessage().getText());
+//		if (dayOfTheWeek == -1) {
+//			smService.getRandomMessage();
+//		}
+//		return gsService.getMessage(dayOfTheWeek);
+//    }
+//    
+//    @EventMapping
+//    public Message handleStickerMessage(MessageEvent<StickerMessageContent> event) {
+//    	return smService.getRandomMessage();
+//    }
+//
+//    @EventMapping
+//    public void handleDefaultMessageEvent(Event event) {
+//        System.out.println("event: " + event);
+//    }
 }
