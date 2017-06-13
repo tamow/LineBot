737125f398cc0ecc3b90f5dd9fdb10d59171d932
渡部保 <tamotsu917@yahoo.co.jp>
2017-06-14 00:32:02 +0900

    add header

--- src/main/java/com/example/service/HiraganaService.java
+++ src/main/java/com/example/service/HiraganaService.java
@@ -1,7 +1,13 @@
 package com.example.service;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,18 +17,21 @@ import com.example.dto.HiraganaResponseDto;
 @Service
 public class HiraganaService {
 
-	public String convertToHiragana(String word) {
+	public String convertToHiragana(String word) throws URISyntaxException {
 
-		String url = "https://labs.goo.ne.jp/api/hiragana";
+		URI uri = new URI("https://labs.goo.ne.jp/api/hiragana");
 
-		HiraganaRequestDto req = new HiraganaRequestDto();
-		req.setAppId("5d6a00e3c13dd4cadde7e775c6440848ef32a696fa6e13b5c1ad23ce10db1b2e");
-		req.setRequestId("record003");
-		req.setOutputType("hiragana");
-		req.setSentence(word);
+		HiraganaRequestDto body = new HiraganaRequestDto();
+		body.setAppId("5d6a00e3c13dd4cadde7e775c6440848ef32a696fa6e13b5c1ad23ce10db1b2e");
+		body.setRequestId("record003");
+		body.setOutputType("hiragana");
+		body.setSentence(word);
 
-		HiraganaResponseDto res = new RestTemplate().postForObject(url, req, HiraganaResponseDto.class);
-		
-		return res.getConverted();
+		RequestEntity<HiraganaRequestDto> req = RequestEntity.post(uri).contentType(MediaType.APPLICATION_JSON)
+				.body(body);
+
+		ResponseEntity<HiraganaResponseDto> res = new RestTemplate().exchange(req, HiraganaResponseDto.class);
+
+		return res.getBody().getConverted();
 	}
 }
