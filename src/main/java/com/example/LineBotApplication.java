346c8cefb75130db1f8117a7c8660c6e8efb15ea
Tamotsu Watanabe <tamotsu917@yahoo.co.jp>
2017-11-13 00:44:53 +0900

    Add AWS

--- src/main/java/com/example/LineBotApplication.java
+++ src/main/java/com/example/LineBotApplication.java
@@ -1,18 +1,31 @@
 package com.example;
 
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.util.IOUtils;
 import com.example.controller.LineBotController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -20,6 +33,9 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @LineMessageHandler
 public class LineBotApplication {
 
+	@Autowired
+    private LineMessagingClient lineMessagingClient;
+	
 	@Autowired
 	private LineBotController controller;
 
@@ -37,6 +53,26 @@ public class LineBotApplication {
 	public Message handleStickerMessage(MessageEvent<StickerMessageContent> event) {
 		return controller.replyStickerMessage();
 	}
+
+	@EventMapping
+    public Message handleImageMessageEvent(MessageEvent<ImageMessageContent> event) {
+		try {
+			InputStream is = lineMessagingClient.getMessageContent(event.getMessage().getId()).get().getStream();
+	        DetectLabelsRequest request = new DetectLabelsRequest();
+			request.withImage(new Image().withBytes(ByteBuffer.wrap(IOUtils.toByteArray(is))));
+	        request.withMaxLabels(5);
+
+			AWSCredentials credential = new ClasspathPropertiesFileCredentialsProvider().getCredentials();
+	        AmazonRekognitionClient client = new AmazonRekognitionClient(credential);
+	        DetectLabelsResult result = client.detectLabels(request);
+	        ObjectMapper objectMapper = new ObjectMapper();
+			return new TextMessage(objectMapper.writeValueAsString(result));
+
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		return new TextMessage("結果なし");
+    }
 
 	@EventMapping
 	public void handleDefaultMessageEvent(Event event) {
