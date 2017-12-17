f9443d07f9cf56c9119fbbca14936db7a0ecaa75
Tamotsu Watanabe <tamotsu917@yahoo.co.jp>
2017-12-17 18:30:16 +0900

    modify

--- src/main/java/com/example/controller/LineBotController.java
+++ src/main/java/com/example/controller/LineBotController.java
@@ -3,12 +3,16 @@ package com.example.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.time.ZonedDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -45,25 +49,30 @@ public class LineBotController {
 	private LuisService luisService;
 	
 	@Autowired
-	private RekognitionService rService;
+	private RekognitionService awsService;
 
 	@Autowired
-	private TranslationService tService;
+	private TranslationService msService;
 
 	public Message replyTextMessage(String text, ZonedDateTime dateTime) throws URISyntaxException {
 
-		LuisResponseDto luisRes = luisService.luis(text);
-		// 予定表
-		if (SCHEDULE_INTENT.equals(luisRes.getTopScoringIntent().getIntent()) && luisRes.getTopScoringIntent().getScore() > 0.9) {
-			return gsService.getSchedule();
-		}
-		Optional<LuisEntity> luisEntity = luisRes.getEntities().stream().filter(p -> "day".equals(p.getType()))
-				.sorted(Comparator.comparingDouble(LuisEntity::getScore).reversed()).findFirst();
-		if (!luisEntity.isPresent()) {
-			return smService.getQuestionMessage();
+		String word = "";
+		if ("今日は？".equals(text)) {
+			word = "今日";
+		} else {
+			LuisResponseDto luisRes = luisService.luis(text);
+			// 予定表
+			if (SCHEDULE_INTENT.equals(luisRes.getTopScoringIntent().getIntent()) && luisRes.getTopScoringIntent().getScore() > 0.9) {
+				return gsService.getSchedule();
+			}
+			Optional<LuisEntity> luisEntity = luisRes.getEntities().stream().filter(p -> "day".equals(p.getType()))
+					.sorted(Comparator.comparingDouble(LuisEntity::getScore).reversed()).findFirst();
+			if (!luisEntity.isPresent()) {
+				return smService.getQuestionMessage();
+			}
+			word = luisEntity.get().getEntity();
 		}
 		// 曜日
-		String word = luisEntity.get().getEntity();
 		int dayOfWeek = waService.getDayOfWeek(word, dateTime);
 		if (dayOfWeek == -1) {
 			return smService.getQuestionMessage();
@@ -77,12 +86,17 @@ public class LineBotController {
 	
 	public Message replyImageMessage(String messageId) throws InterruptedException, ExecutionException, IOException {
 		InputStream is = lineMessagingClient.getMessageContent(messageId).get().getStream();
-		List<String> items = rService.analyze(is);
+		List<String> items = awsService.analyze(is);
 		
 		String res = "";
 		for (String item : items) {
-			res += tService.translate(item) + System.getProperty("line.separator");
-		}
+			String url = "http://cgi.city.yokohama.lg.jp/shigen/bunbetsu/search2.html?txt=" + URLEncoder.encode(msService.translate(item), "UTF-8") + "&lang=ja";
+			Document document = Jsoup.connect(url).get();
+			Elements elements = document.getElementsByClass("item_desc");
+			if (elements.size() >= 2) {
+				res += item + ": " +elements.get(1) + System.getProperty("line.separator");
+			}
+		}		
 		return new TextMessage(res);
 	}
 }
